e0efdd76e3ae8d93a87824a658eed806514ab13f
libra guest <magalhas@gmail.com>
2012-09-28 18:43:51 -0400

    now compatible with httpd-mocl 0.2.2

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+
+"use strict";
 var commander = require('commander'),
     phantom = require('phantom'),
     HttpdMock = require('httpd-mock'),
@@ -6,48 +8,43 @@ var commander = require('commander'),
 
 if (!module.parent) {
     commander
-        .version('0.1.1')
+        .version('0.1.2')
         .option('-c, --config <path>', 'Path to the configuration file')
         .option('-t, --test-html-file <filename>', "Uri of your Jasmine's html spec runner. Usually it's just a filename if it's placed on the root folder of the config file")
         .parse(process.argv);
-    
+
     if (!commander.config) {
         console.log('Must specify configuration path using -c <path> or --config <path>');
         process.exit();
     }
-    
+
     commander.testHtmlFile = commander.testHtmlFile || 'tests.html';
-    
-    httpdMock
-        .setConfigFile(commander.config)
-        .setServerRootPath()
-        .createWebServices()
-        .start();
-    
+
+    httpdMock.setConfigFile(commander.config).setServerRootPath().createWebServices().start();
+
     console.log('Starting jasmine\'s html runner');
-    phantom.create(function (ph) {
-    	return ph.createPage(function (page) {
-    		return page.open('http://localhost:' + httpdMock.getPort() + '/' + commander.testHtmlFile, function (status) {
-    			if (!status) {
-    				console.log('Jasmine\'s html runner failed to access the server');
-    				ph.exit();
+    phantom.create(function(ph) {
+        return ph.createPage(function(page) {
+            return page.open('http://localhost:' + httpdMock.getPort() + '/' + commander.testHtmlFile, function(status) {
+                if (!status) {
+                    console.log('Jasmine\'s html runner failed to access the server');
+                    ph.exit();
                     return process.exit();
-    			}
-    			console.log('Jasmine\'s runner client waiting for tests to finish...');
-    			setInterval(function () {
-    				page.evaluate(function () {
-    					var element = document && document.querySelector
-    							&& document.querySelector('.runner span .description');
-    					return (element && element.innerText) || '';
-    				}, function (result) {
-    					if (result.length > 0) {
-    						console.log(result);
+                }
+                console.log('Jasmine\'s runner client waiting for tests to finish...');
+                setInterval(function() {
+                    page.evaluate(function() {
+                        var element = document && document.querySelector && document.querySelector('.runner span .description');
+                        return (element && element.innerText) || '';
+                    }, function(result) {
+                        if (result.length > 0) {
+                            console.log(result);
                             ph.exit();
-    						return process.exit();
-    					}
-    				});
-    			}, 1000);
-    		});
-    	});
+                            return process.exit();
+                        }
+                    });
+                }, 1000);
+            });
+        });
     });
 }
