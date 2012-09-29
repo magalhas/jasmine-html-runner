46905227f30c088ff17962f97e274dcd60fa047b
libra guest <magalhas@gmail.com>
2012-09-28 22:13:01 -0400

    changed arguments doc and help output when needed

--- index.js
+++ index.js
@@ -1,41 +1,46 @@
 #!/usr/bin/env node
-
 "use strict";
-var commander = require('commander'),
-    phantom = require('phantom'),
-    HttpdMock = require('httpd-mock'),
+
+var jspackage = require("./package.json"),
+    commander = require("commander"),
+    phantom = require("phantom"),
+    HttpdMock = require("httpd-mock"),
     httpdMock = new HttpdMock();
 
 if (!module.parent) {
     commander
-        .version('0.1.2')
-        .option('-c, --config <path>', 'Path to the configuration file')
-        .option('-t, --test-html-file <filename>', "Uri of your Jasmine's html spec runner. Usually it's just a filename if it's placed on the root folder of the config file")
+        .version(jspackage.version)
+        .option("-c, --config <path>", "Path to the configuration file")
+        .option("-t, --test-html-file <filename>", "Uri of your Jasmine's html spec runner [tests.html]", String, "tests.html")
         .parse(process.argv);
 
     if (!commander.config) {
-        console.log('Must specify configuration path using -c <path> or --config <path>');
+        console.error("Must use argument --config <path>");
+        commander.outputHelp();
         process.exit();
     }
 
-    commander.testHtmlFile = commander.testHtmlFile || 'tests.html';
+    httpdMock
+        .setConfigFile(commander.config)
+        .setServerRootPath()
+        .createWebServices()
+        .start();
 
-    httpdMock.setConfigFile(commander.config).setServerRootPath().createWebServices().start();
-
-    console.log('Starting jasmine\'s html runner');
+    console.log("Starting jasmine's html runner");
     phantom.create(function(ph) {
         return ph.createPage(function(page) {
-            return page.open('http://localhost:' + httpdMock.getPort() + '/' + commander.testHtmlFile, function(status) {
+            return page.open("http://localhost:" + httpdMock.getPort() + "/" + commander.testHtmlFile, function(status) {
                 if (!status) {
-                    console.log('Jasmine\'s html runner failed to access the server');
+                    console.log("Jasmine's html runner failed to access the server");
                     ph.exit();
                     return process.exit();
                 }
-                console.log('Jasmine\'s runner client waiting for tests to finish...');
+                console.log("Jasmine's runner client waiting for tests to finish...");
                 setInterval(function() {
                     page.evaluate(function() {
-                        var element = document && document.querySelector && document.querySelector('.runner span .description');
-                        return (element && element.innerText) || '';
+                        var element = document && document.querySelector
+                            && document.querySelector(".runner span .description");
+                        return (element && element.innerText) || "";
                     }, function(result) {
                         if (result.length > 0) {
                             console.log(result);
