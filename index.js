834b54e8794d32c354d40eb4477cd696f02948e3
OpenShift guest <magalhas@gmail.com>
2013-01-28 07:53:43 -0500

    Support to jUnit XML Output. Requires specific jasmine spec runner

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 
 var jspackage = require("./package.json"),
     commander = require("commander"),
+    fs = require("fs"),
     phantom = require("phantom"),
     HttpdMock = require("httpd-mock"),
-    httpdMock = new HttpdMock();
+    httpdMock = new HttpdMock(),
+    config;
 
 if (!module.parent) {
     commander
@@ -19,6 +21,7 @@ if (!module.parent) {
         commander.outputHelp();
         process.exit();
     }
+    config = require(commander.config);
 
     httpdMock
         .setConfigFile(commander.config)
@@ -28,7 +31,13 @@ if (!module.parent) {
 
     console.log("Starting jasmine's html runner");
     phantom.create(function(ph) {
+        var jUnitXMLOutput = "";
         return ph.createPage(function(page) {
+            page.set("onConsoleMessage", function (msg) {
+                if (msg.indexOf("<<jUnit") === 0) {
+                    jUnitXMLOutput += msg.substr(7);
+                }
+            });
             return page.open("http://localhost:" + httpdMock.getPort() + "/" + commander.testHtmlFile, function(status) {
                 if (!status) {
                     console.log("Jasmine's html runner failed to access the server");
@@ -36,14 +45,21 @@ if (!module.parent) {
                     return process.exit();
                 }
                 console.log("Jasmine's runner client waiting for tests to finish...");
-                setInterval(function() {
-                    page.evaluate(function() {
-                        var element = document && document.querySelector
-                            && document.querySelector(".runner span .description");
-                        return (element && element.innerText) || "";
-                    }, function(result) {
+                setInterval(function () {
+                    page.evaluate(function () {
+                        var element = document && document.querySelector && document.querySelector('.runner span .description');
+                        return (element && element.innerText) || '';
+                    }, function (result) {
                         if (result.length > 0) {
                             console.log(result);
+                            if (config.outputFile && jUnitXMLOutput.length > 0) {
+                                jUnitXMLOutput = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<testsuites>" + jUnitXMLOutput + "\n</testsuites>";
+                                var fd = fs.openSync(config.outputFile, "w");
+                                fs.writeSync(fd, jUnitXMLOutput, 0);
+                                fs.closeSync(fd);
+                            } else if (!config.outputFile) {
+                                console.log("Results output file not specified. File not saved.");
+                            }
                             ph.exit();
                             return process.exit();
                         }
